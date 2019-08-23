34c3b6f740aa3d00775be0af3dfff14c083aa524
jbeisman <jbeisman@lanl.gov>
2019-08-22 19:13:33 -0600

    added python GetXY helper function

---
+++ PF_GetXY.py
@@ -0,0 +1,22 @@
+def GetXY_ParFlow_1D_100(path,root,pf_file):
+
+    # read CrunchFlow data
+    filename = os.path.join(path,cf_file)
+    f = open(filename,'r')
+    lines = f.readlines()
+    f.close()
+
+    # ignore couple of lines
+    for i in range(1):
+      lines.pop(0)
+
+    # extract data x0, x1, ..., xN-1 per line, keep only two columns
+    xv=[]
+    yv=[] 
+    for line in lines:
+      yv = yv + [float(line.split()[0])]
+    
+    xv = np.linspace(0.5,99.5,100)
+    yv = np.array(yv)
+
+    return (xv, yv)
